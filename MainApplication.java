534b63ef908e310dc916a05f113464a271122994
IamSharma <111356959+srikanthsharm@users.noreply.github.com>
2026-08-24 17:42:27 +0530

    Create MainApplication with Spring Boot setup

---
+++ MainApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Azure DevOps Pipeline!";
+    }
+}
